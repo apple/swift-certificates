4feb081b420104d3e24fad51e32ea403f6d20e69
David Nadoba <d_nadoba@apple.com>
2022-11-30 11:28:42 +0100

    Fix soundness
    File extension of the CONTRIBUTORS file is `.txt` not `.md`.
    Add missing license header to some files.

--- Sources/X509/Verifier/CertificateStore.swift
+++ Sources/X509/Verifier/CertificateStore.swift
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftCertificates open source project
+//
+// Copyright (c) 2022 Apple Inc. and the SwiftCertificates project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftCertificates project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 /// A collection of ``Certificate`` objects for use in a verifier.
 public struct CertificateStore: Sendable, Hashable {
     /// Stores the certificates, indexed by DistinguishedName.
