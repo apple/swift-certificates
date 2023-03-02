73f1ba10d6fe9a6b3049be0ab491528e7b9ab2d8
Yim Lee <yim_lee@apple.com>
2023-03-02 13:25:54 -0800

    Use OSS swift-asn1 (#28)

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 //
 // This source file is part of the SwiftCertificates open source project
 //
-// Copyright (c) 2022 Apple Inc. and the SwiftCertificates project authors
+// Copyright (c) 2022-2023 Apple Inc. and the SwiftCertificates project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
@@ -30,9 +30,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-crypto.git", from: "2.2.1"),
-        // swift-asn1 repo is private, so we can't access it anonymously yet
-        // .package(url: "https://github.com/apple/swift-asn1.git", .upToNextMinor(from: "0.6.0")),
-        .package(url: "git@github.com:apple/swift-asn1.git", .upToNextMinor(from: "0.6.0")),
+        .package(url: "https://github.com/apple/swift-asn1.git", .upToNextMinor(from: "0.6.0")),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
