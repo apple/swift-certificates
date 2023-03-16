83af4ff3cadeeb3168a009b83990df3d0353d57f
Cory Benfield <lukasa@apple.com>
2023-03-16 15:37:20 +0000

    Add directory name constraints. (#44)
    Just to round things out we can add directory name constraints. There's
    nominally a fairly complex algorithm for validating directory name
    constraints, but most implementations don't bother, instead falling back
    to direct matches.

---
+++ Tests/X509Tests/NameConstraintsTests.swift
@@ -0,0 +1,51 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftCertificates open source project
+//
+// Copyright (c) 2023 Apple Inc. and the SwiftCertificates project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftCertificates project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import XCTest
+import SwiftASN1
+@testable import X509
+
+final class NameConstraintsTests: XCTestCase {
+    func testDirectoryNameMatches() throws {
+        // The key here is that a distinguished name only matches a constraint if they're equal.
+        let names: [DistinguishedName] = [
+            try DistinguishedName {
+                CountryName("US")
+                StateOrProvinceName("CA")
+                OrganizationName("Apple")
+            },
+            try DistinguishedName {
+                CountryName("US")
+                StateOrProvinceName("CA")
+                OrganizationName("Apple")
+                CommonName("Test")
+            },
+            try DistinguishedName {
+                CountryName("GB")
+                OrganizationName("Apple")
+                CommonName("Test")
+            },
+        ]
+
+        for firstName in names {
+            for secondName in names {
+                XCTAssertEqual(
+                    NameConstraintsPolicy.directoryNameMatchesConstraint(directoryName: firstName, constraint: secondName),
+                    firstName == secondName,
+                    "Expected directory name match to be \(firstName == secondName) for \(firstName) and \(secondName)"
+                )
+            }
+        }
+    }
+}
