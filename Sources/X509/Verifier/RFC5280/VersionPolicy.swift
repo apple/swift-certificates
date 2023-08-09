272a0803621371dde8f857fb5fee16426a2625bb
David Nadoba <d_nadoba@apple.com>
2023-08-09 17:29:35 +0100

    Reject `v1` `Certificate`s with extensions
    v1 certificates do not support extensions and any v1 certificate we encounter with extensions can’t be used to verify a chain.

---
+++ Sources/X509/Verifier/RFC5280/VersionPolicy.swift
@@ -0,0 +1,35 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftCertificates open source project
+//
+// Copyright (c) 2023 Apple Inc. and the SwiftCertificates project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftCertificates project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import SwiftASN1
+
+/// A sub-policy of the ``RFC5280Policy`` that polices that version 1 certificates do not contain extensions.
+@usableFromInline
+struct VersionPolicy: VerifierPolicy {
+    @inlinable
+    var verifyingCriticalExtensions: [SwiftASN1.ASN1ObjectIdentifier] { [] }
+    
+    @inlinable
+    init() {}
+    
+    @inlinable
+    func chainMeetsPolicyRequirements(chain: UnverifiedCertificateChain) -> PolicyEvaluationResult {
+        for certificate in chain {
+            if certificate.version == .v1 && certificate.extensions.isEmpty == false {
+                return .failsToMeetPolicy(reason: "version 1 certificate contains extensions but should not: \(certificate)")
+            }
+        }
+        return .meetsPolicy
+    }
+}
