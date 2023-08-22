8005d751f14822afabb9fb63e2432224b6e10e74
David Nadoba <d_nadoba@apple.com>
2023-08-22 15:29:13 +0100

    install jemalloc

--- benchmarks/Package.swift
+++ benchmarks/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .macOS(.v13),
     ],
     dependencies: [
-        .package(name: "swift-certificates", path: "../"),
+        .package(path: "../"),
         .package(url: "https://github.com/ordo-one/package-benchmark", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "2.5.0"),
         .package(url: "https://github.com/apple/swift-asn1.git", .upToNextMinor(from: "0.10.0")),
