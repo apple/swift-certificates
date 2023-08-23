5a038c7a4aa2a8565e27ca968c8df3443e5a7a7d
David Nadoba <d_nadoba@apple.com>
2023-08-23 15:23:27 +0100

    use automated threshold import

--- benchmarks/Benchmarks/CertificatesBenchmarks/Benchmarks.swift
+++ benchmarks/Benchmarks/CertificatesBenchmarks/Benchmarks.swift
@@ -16,61 +16,35 @@ import Benchmark
 import Sources
 import Foundation
 
-func thresholds(for benchmarkName: String) -> [BenchmarkMetric : BenchmarkThresholds]? {
-    // https://forums.swift.org/t/pitch-introduce-module-to-get-the-current-module-name/45806/8
-    let moduleName = String("\(#fileID)".prefix(while: { $0 != "/" }))
-    
-    return BenchmarkThresholds.makeBenchmarkThresholds(
-        path: FileManager.default.currentDirectoryPath,
-        moduleName: moduleName,
-        benchmarkName: benchmarkName
-    )
-}
-
-
 
 let benchmarks = {
     Benchmark.defaultConfiguration = .init(
         metrics: [.mallocCountTotal, .syscalls, .retainCount],
         warmupIterations: 1
     )
-    
-    do {
-        let testName = "Verifier"
-        Benchmark(testName, configuration: .init(
-            metrics: [.mallocCountTotal, .syscalls],
-            thresholds: thresholds(for: testName))
-        ) { benchmark in
-            for _ in benchmark.scaledIterations {
-                await verifier()
-            }
-        }
-    }
 
-    do {
-        let runParseWebPKIRoots = parseWebPKIRoots()
-        let testName = "Parse WebPKI Roots"
-        Benchmark(testName, configuration: .init(thresholds: thresholds(for: testName))) { benchmark in
-            for _ in benchmark.scaledIterations {
-                runParseWebPKIRoots()
-            }
-        }
-    }
-    do {
-        let testName = "TinyArray non-allocating functions"
-        Benchmark(testName, configuration: .init(thresholds: thresholds(for: testName))) { benchmark in
-            for _ in benchmark.scaledIterations {
-                tinyArrayNonAllocationFunctions()
-            }
+    Benchmark("Verifier", configuration: .init(metrics: [.mallocCountTotal, .syscalls])) { benchmark in
+        for _ in benchmark.scaledIterations {
+            await verifier()
         }
     }
     
-    do {
-        let testName = "TinyArray.append(_:)"
-        Benchmark(testName, configuration: .init(thresholds: thresholds(for: testName))) { benchmark in
-            for _ in benchmark.scaledIterations {
-                tinyArrayAppend()
-            }
+    let runParseWebPKIRoots = parseWebPKIRoots()
+    Benchmark("Parse WebPKI Roots") { benchmark in
+        for _ in benchmark.scaledIterations {
+            runParseWebPKIRoots()
+        }
+    }
+    
+    Benchmark("TinyArray non-allocating functions") { benchmark in
+        for _ in benchmark.scaledIterations {
+            tinyArrayNonAllocationFunctions()
+        }
+    }
+    
+    Benchmark("TinyArray.append(_:)") { benchmark in
+        for _ in benchmark.scaledIterations {
+            tinyArrayAppend()
         }
     }
 }
