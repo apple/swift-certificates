5d64d20c24b570b01e3177f25c71c77f32bdfe58
David Nadoba <d_nadoba@apple.com>
2023-08-24 10:45:48 +0100

    note why we reverse the intermediates

--- Sources/X509/Verifier/Verifier.swift
+++ Sources/X509/Verifier/Verifier.swift
@@ -120,6 +120,9 @@ public struct Verifier<Policy: VerifierPolicy> {
                     )
                 )
 
+                // we need to reverse the order of the already sorted intermediates because
+                // we will push them on to the `partialChains` stack which in turn will
+                // consume them in the reverse order the have been pushed onto the stack
                 for parent in intermediateParents.reversed() {
                     if self.shouldSkipAddingCertificate(
                         partialChain: nextPartialCandidate,
