246d84b0677b0ec5684acb5cad5d201beb2244b1
David Nadoba <d_nadoba@apple.com>
2023-08-24 14:47:03 +0100

    Use beta release of `swift-asn1`

--- Package.swift
+++ Package.swift
@@ -75,7 +75,7 @@ let package = Package(
 if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
     package.dependencies += [
         .package(url: "https://github.com/apple/swift-crypto.git", from: "2.5.0"),
-        .package(url: "https://github.com/apple/swift-asn1.git", .upToNextMinor(from: "0.10.0")),
+        .package(url: "https://github.com/apple/swift-asn1.git", from: "1.0.0-beta.1"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ]
 } else {
