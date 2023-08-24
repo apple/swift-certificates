8936fd8eb708582cfc2c665ad71978c5c96bf79a
David Nadoba <d_nadoba@apple.com>
2023-08-24 16:08:27 +0100

    update swift-asn1 in integeration tests as well

--- IntegrationTests/tests_01_allocation_counters/test_01_resources/run-swift-certificates-alloc-counter-tests.sh
+++ IntegrationTests/tests_01_allocation_counters/test_01_resources/run-swift-certificates-alloc-counter-tests.sh
@@ -50,5 +50,5 @@ fi
     -m Crypto \
     -s "$here/shared.swift" \
     -t "$tmp_dir" \
-    -d <( echo '.package(url: "https://github.com/apple/swift-asn1.git", from: "0.9.1"),.package(url: "https://github.com/apple/swift-crypto.git", from: "2.5.0"),' ) \
+    -d <( echo '.package(url: "https://github.com/apple/swift-asn1.git", from: "1.0.0-beta.1"),.package(url: "https://github.com/apple/swift-crypto.git", from: "2.5.0"),' ) \
     "${tests_to_run[@]}"
