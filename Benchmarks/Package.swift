1c6fdaaa963331efd9b24e12f3f48fbede4c6990
David Nadoba <d_nadoba@apple.com>
2023-09-25 09:57:58 +0200

    Update to `swift-asn1` `1.0.0`
    and use `.git` suffix consistently for dependencies

--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -22,9 +22,9 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../"),
-        .package(url: "https://github.com/ordo-one/package-benchmark", from: "1.11.1"),
+        .package(url: "https://github.com/ordo-one/package-benchmark.git", from: "1.11.1"),
         .package(url: "https://github.com/apple/swift-crypto.git", "2.5.0" ..< "4.0.0"),
-        .package(url: "https://github.com/apple/swift-asn1.git", from: "1.0.0-beta.2"),
+        .package(url: "https://github.com/apple/swift-asn1.git", from: "1.0.0"),
     ],
     targets: [
         .executableTarget(
