7f32522270e775eb64a74b5fe34e14d995cf79ca
Jon C. Thomason <2807816+jonct@users.noreply.github.com>
2024-11-22 01:14:55 -0500

    Test verifying an attached signature and timestamp

--- Tests/X509Tests/CMSTests.swift
+++ Tests/X509Tests/CMSTests.swift
@@ -1012,6 +1012,27 @@ final class CMSTests: XCTestCase {
         XCTAssertValidSignature(isValidSignature)
     }
 
+    func testSigningAttachedWithSigningTimeSignedAttr() async throws {
+        let data: [UInt8] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        let signature = try CMS.sign(
+            data,
+            signatureAlgorithm: .ecdsaWithSHA256,
+            certificate: Self.leaf1Cert,
+            privateKey: Self.leaf1Key,
+            signingTime: Date(),
+            detached: false
+        )
+        let isValidSignature = await CMS.isValidSignature(
+            dataBytes: data,
+            signatureBytes: signature,
+            trustRoots: CertificateStore([Self.rootCert]),
+            allowAttachedContent: true
+        ) {
+            Self.defaultPolicies
+        }
+        XCTAssertValidSignature(isValidSignature)
+    }
+
     func testSigningContentBytesWithSigningTimeSignedAttrsIsInvalidSignature() async throws {
         let data: [UInt8] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
