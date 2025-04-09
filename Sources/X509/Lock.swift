63d06f4504913a4eb1112c71e4a77ad35055a829
3405691582 <dsk@google.com>
2025-04-09 06:35:15 -0400

    Make the usual pthread changes for OpenBSD. (#239)
    Co-authored-by: David Nadoba <d_nadoba@apple.com>
    Co-authored-by: Cory Benfield <lukasa@apple.com>

--- Sources/X509/Lock.swift
+++ Sources/X509/Lock.swift
@@ -35,6 +35,9 @@ import wasi_pthread
 #if os(Windows)
 @usableFromInline
 typealias LockPrimitive = SRWLOCK
+#elseif os(OpenBSD)
+@usableFromInline
+typealias LockPrimitive = pthread_mutex_t?
 #else
 @usableFromInline
 typealias LockPrimitive = pthread_mutex_t
@@ -51,10 +54,18 @@ extension LockOperations {
         #if os(Windows)
         InitializeSRWLock(mutex)
         #elseif (compiler(<6.1) && !os(WASI)) || (compiler(>=6.1) && _runtime(_multithreaded))
+        #if os(OpenBSD)
+        var attr = pthread_mutexattr_t(bitPattern: 0)
+        #else
         var attr = pthread_mutexattr_t()
+        #endif
         pthread_mutexattr_init(&attr)
         debugOnly {
+            #if os(OpenBSD)
+            pthread_mutexattr_settype(&attr, .init(PTHREAD_MUTEX_ERRORCHECK.rawValue))
+            #else
             pthread_mutexattr_settype(&attr, .init(PTHREAD_MUTEX_ERRORCHECK))
+            #endif
         }
 
         let err = pthread_mutex_init(mutex, &attr)
