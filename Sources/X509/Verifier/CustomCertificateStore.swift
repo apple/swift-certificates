4dc9dfc06b02dc6a95db35ce03b71b99a226e07a
Brian Maher <be_maher@apple.com>
2025-05-29 09:43:43 -0700

    Create a CustomCertificateStore protocol.
    This new protocol allows one to customize the logic for matching an
    issuer to the subject certificate authority.

---
+++ Sources/X509/Verifier/CustomCertificateStore.swift
@@ -0,0 +1,116 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftCertificates open source project
+//
+// Copyright (c) 2022 Apple Inc. and the SwiftCertificates project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftCertificates project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+/// Implement the ``CustomCertificateStore`` if you want to perform dynamic
+/// certificate lookup, or if you need custom logic when matching the
+/// ``DistinguishedName`` of an Issuer with the Subject of the issuer
+/// certificate.
+@available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, macCatalyst 13, visionOS 1.0, *)
+public protocol CustomCertificateStore: Sendable, Hashable {
+    /// Obtain a list of certificates which has a given subject.
+    subscript(subject: DistinguishedName) -> [Certificate]? {
+        get
+    }
+
+    /// Validate if a given certificate is known to exist in this certificate
+    /// store.
+    func contains(_ certificate: Certificate) -> Bool
+
+    /// Add a certificate to this certificate store.
+    mutating func append(contentsOf certificates: some Sequence<Certificate>)
+}
+
+@available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, macCatalyst 13, visionOS 1.0, *)
+@usableFromInline
+struct AnyCustomCertificateStore: CustomCertificateStore {
+    @usableFromInline
+    var value: any DynCustomCertificateStore
+
+    @usableFromInline
+    init<T: CustomCertificateStore>(_ value: T) {
+        self.value = Backing(value)
+    }
+
+    @inlinable
+    subscript(subject: DistinguishedName) -> [Certificate]? {
+        value[subject]
+    }
+
+    @inlinable
+    func contains(_ certificate: Certificate) -> Bool {
+        value.contains(certificate)
+    }
+
+    @inlinable
+    mutating func append(contentsOf certificates: some Sequence<Certificate>) {
+        value.append(contentsOf: certificates)
+    }
+}
+
+@available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, macCatalyst 13, visionOS 1.0, *)
+extension AnyCustomCertificateStore: Hashable {
+    public static func == (lhs: AnyCustomCertificateStore, rhs: AnyCustomCertificateStore) -> Bool {
+        return lhs.value.isEqual(rhs.value, recurse: true)
+    }
+
+    public var hashValue: Int {
+        return value.hashValue
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        value.hash(into: &hasher)
+    }
+}
+
+@available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, macCatalyst 13, visionOS 1.0, *)
+extension AnyCustomCertificateStore {
+    @usableFromInline
+    protocol DynCustomCertificateStore: CustomCertificateStore {
+        func isEqual(_ rhs: any DynCustomCertificateStore, recurse: Bool) -> Bool
+    }
+}
+
+@available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, macCatalyst 13, visionOS 1.0, *)
+extension AnyCustomCertificateStore {
+    struct Backing<T: CustomCertificateStore>: DynCustomCertificateStore {
+        var value: T
+
+        init(_ value: T) {
+            self.value = value
+        }
+
+        subscript(subject: DistinguishedName) -> [Certificate]? {
+            value[subject]
+        }
+
+        func contains(_ certificate: Certificate) -> Bool {
+            value.contains(certificate)
+        }
+
+        @inlinable
+        mutating func append(contentsOf certificates: some Sequence<Certificate>) {
+            value.append(contentsOf: certificates)
+        }
+
+        func isEqual(_ rhs: any DynCustomCertificateStore, recurse: Bool) -> Bool {
+            guard let rhs = rhs as? Self else {
+                guard recurse else {
+                    return false
+                }
+                return rhs.isEqual(self, recurse: false)
+            }
+            return self == rhs
+        }
+    }
+}
