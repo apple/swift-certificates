e8c95ad095546f89231839595a7b715abee2dbaf
Justin Oroz <justinoroz@apple.com>
2025-10-09 01:30:02 -0700

    Add CSR initializers

--- Sources/X509/CSR/CertificateSigningRequest.swift
+++ Sources/X509/CSR/CertificateSigningRequest.swift
@@ -150,6 +150,82 @@ public struct CertificateSigningRequest {
         self.signatureBytes = try DER.Serializer.serialized(element: ASN1BitString(self.signature))[...]
     }
 
+    /// Construct a CSR for a signer.
+    ///
+    /// This API can be used to construct a certificate signing request that can be passed to a certificate
+    /// authority. It will correctly generate a signature over the request.
+    ///
+    /// - Parameters:
+    ///   - version: The CSR version.
+    ///   - subject: The ``DistinguishedName`` of the subject of this CSR
+    ///   - publicKey: The public key associated with this CSR.
+    ///   - signer: The signer associated with this CSR.
+    ///   - attributes: The attributes associated with this CSR
+    ///   - signatureAlgorithm: The signature algorithm to use for the signature on this CSR.
+    @inlinable
+    public init(
+        version: Version,
+        subject: DistinguishedName,
+        publicKey: Certificate.PublicKey,
+        signer: some Certificate.Signer,
+        attributes: Attributes,
+        signatureAlgorithm: Certificate.SignatureAlgorithm
+    ) throws {
+        self.info = CertificationRequestInfo(
+            version: version,
+            subject: subject,
+            publicKey: publicKey,
+            attributes: attributes
+        )
+        self.signatureAlgorithm = signatureAlgorithm
+
+        let infoBytes = try DER.Serializer.serialized(element: self.info)
+        self.signature = try signer.sign(bytes: infoBytes, signatureAlgorithm: signatureAlgorithm)
+        self.infoBytes = infoBytes[...]
+        self.signatureAlgorithmBytes = try DER.Serializer.serialized(
+            element: AlgorithmIdentifier(self.signatureAlgorithm)
+        )[...]
+        self.signatureBytes = try DER.Serializer.serialized(element: ASN1BitString(self.signature))[...]
+    }
+
+    /// Construct a CSR for a signer.
+    ///
+    /// This API can be used to construct a certificate signing request that can be passed to a certificate
+    /// authority. It will correctly generate a signature over the request.
+    ///
+    /// - Parameters:
+    ///   - version: The CSR version.
+    ///   - subject: The ``DistinguishedName`` of the subject of this CSR
+    ///   - publicKey: The public key associated with this CSR.
+    ///   - signer: The signer associated with this CSR.
+    ///   - attributes: The attributes associated with this CSR
+    ///   - signatureAlgorithm: The signature algorithm to use for the signature on this CSR.
+    @inlinable
+    public init(
+        version: Version,
+        subject: DistinguishedName,
+        publicKey: Certificate.PublicKey,
+        signer: some Certificate.AsyncSigner,
+        attributes: Attributes,
+        signatureAlgorithm: Certificate.SignatureAlgorithm
+    ) async throws {
+        self.info = CertificationRequestInfo(
+            version: version,
+            subject: subject,
+            publicKey: publicKey,
+            attributes: attributes
+        )
+        self.signatureAlgorithm = signatureAlgorithm
+
+        let infoBytes = try DER.Serializer.serialized(element: self.info)
+        self.signature = try await signer.sign(bytes: infoBytes, signatureAlgorithm: signatureAlgorithm)
+        self.infoBytes = infoBytes[...]
+        self.signatureAlgorithmBytes = try DER.Serializer.serialized(
+            element: AlgorithmIdentifier(self.signatureAlgorithm)
+        )[...]
+        self.signatureBytes = try DER.Serializer.serialized(element: ASN1BitString(self.signature))[...]
+    }
+
     /// Construct a CSR for a specific private key.
     ///
     /// This API can be used to construct a certificate signing request that can be passed to a certificate
