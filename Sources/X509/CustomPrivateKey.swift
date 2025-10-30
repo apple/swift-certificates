327fd88229270c69272199977d475fa242495329
Justin Oroz <justinoroz@apple.com>
2025-10-30 10:00:01 -0700

    always import import SwiftASN1 for CustomPrivateKey

--- Sources/X509/CustomPrivateKey.swift
+++ Sources/X509/CustomPrivateKey.swift
@@ -16,8 +16,8 @@
 import FoundationEssentials
 #else
 import Foundation
-import SwiftASN1
 #endif
+import SwiftASN1
 
 @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, macCatalyst 13, visionOS 1.0, *)
 public protocol CustomPrivateKey: Sendable, Hashable, PEMSerializable {
