328595a25f38b01aac3e102f034996faeb5ccd4b
Raphael Hiesgen <r_hiesgen@apple.com>
2026-06-30 21:13:45 +0100

    Rename benchmark package

--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../"),
-        .package(url: "https://github.com/ordo-one/package-benchmark.git", from: "1.11.1"),
+        .package(url: "https://github.com/ordo-one/benchmark.git", from: "1.11.1"),
         .package(url: "https://github.com/apple/swift-crypto.git", "3.12.3"..<"5.0.0"),
         .package(url: "https://github.com/apple/swift-asn1.git", from: "1.0.0"),
     ],
@@ -30,7 +30,7 @@ let package = Package(
         .executableTarget(
             name: "CertificatesBenchmark",
             dependencies: [
-                .product(name: "Benchmark", package: "package-benchmark"),
+                .product(name: "Benchmark", package: "benchmark"),
                 .product(name: "X509", package: "swift-certificates"),
                 .product(name: "SwiftASN1", package: "swift-asn1"),
                 .product(name: "Crypto", package: "swift-crypto"),
@@ -40,7 +40,7 @@ let package = Package(
                 .copy("ca-certificates/")
             ],
             plugins: [
-                .plugin(name: "BenchmarkPlugin", package: "package-benchmark")
+                .plugin(name: "BenchmarkPlugin", package: "benchmark")
             ]
         )
     ]
